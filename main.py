18e4924396120e1254671c91f2e318ff57ad61c5
anthony <anthonypaul@nyu.edu>
2024-12-30 19:10:40 -0500

    Initial ingestion utils added

---
+++ main.py
@@ -0,0 +1,62 @@
+import argparse
+import os
+import sys
+from SpotifyPreprocessor import SpotifyPreprocessor
+
+def main():
+
+    
+    script_dir = os.path.dirname(os.path.realpath(__file__)) # need a default folder path directory
+
+    parser = argparse.ArgumentParser(description="Process Spotify Extended Streaming History.") # parser for main
+
+    # arg for extended data folder path. defaults to self
+    parser.add_argument(
+        "folder_path",
+        nargs='?',
+        default=script_dir,
+        help="Path to the folder containing JSON files. Defaults to the script's directory."
+    )
+
+    # arg for  the pickle location
+    parser.add_argument(
+        "--pickle_path",
+        help="Path to the folder for the pickle file. Defaults to the script's directory if not provided."
+    )
+
+    # optional arg for pickle name
+    parser.add_argument(
+        "--pickle_name",
+        default="mycombinedstreaminghistory.pkl",
+        help='Name of the pickle file. Defaults to "mycombinedstreaminghistory.pkl".'
+    )
+
+    args = parser.parse_args()
+
+    # check if the folder path is real
+    if not os.path.isdir(args.folder_path):
+        print(f"Error: The folder path '{args.folder_path}' does not exist or is not a directory.")
+        sys.exit(1)
+
+
+    # still no pickles!
+    if args.pickle_path:
+        if os.path.isdir(args.pickle_path):
+            pickle_full_path = os.path.join(args.pickle_path, args.pickle_name)
+        else:
+            pickle_full_path = args.pickle_path
+    else:
+        pickle_full_path = os.path.join(script_dir, args.pickle_name)
+
+    # check for the json files in the selected folder path
+    json_files = [file for file in os.listdir(args.folder_path) if file.lower().endswith('.json')]
+    if not json_files:
+        print(f"No JSON files found in the specified folder: '{args.folder_path}'.")
+        sys.exit(1)
+
+    # finally process the data
+    df = SpotifyPreprocessor(args.folder_path, pickle_full_path)
+    print("DataFrame processing complete. Shape:", df.shape)
+
+if __name__ == "__main__":
+    main()
--- readme.md
+++ readme.md
@@ -3,3 +3,18 @@
 ## A package for aggregating, analyzing, and preparing your music history.
 
 This is a library of functions for handling your data produced from Spotify's Extended Streaming History dataset for you. 
+
+More tools are to be added, however at this time, only import is ready for public use.
+
+## Importing Files 
+
+A utility for file importing is designed to prepare the data in a manner that tabulates all Extended Streaming History Data into a single Pandas DataFrame. This utility is the first step in the `main.py` file. A required `folder_path` argument is required to point the program towards your data. There are additional arguments, please see the help of the argument for more detail.
+
+An example usage is the following:
+
+```bash
+python main.py /Users/fleshmetal/Documents/Datasets/extendedstreaminghistory
+```
+
+This will produce a pickle file with default name *myextendedstreaminghistory.pkl*. This pickle file will be default referenced from the root folder of the util repo. However, data observation utilities will allow for specific location referencing, if you choose to move the dataset around. 
+
